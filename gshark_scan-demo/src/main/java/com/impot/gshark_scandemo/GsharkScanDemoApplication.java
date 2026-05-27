b7f7795d926fe263525b0d9710ea37e19ef4defd
raochangqi <changqi.rao@nancal.com>
2026-05-27 14:23:23 +0800

    feat:submit

---
+++ gshark_scan-demo/src/main/java/com/impot/gshark_scandemo/GsharkScanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.impot.gshark_scandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsharkScanDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsharkScanDemoApplication.class, args);
+    }
+
+}
---
+++ gshark_scan-demo/src/test/java/com/impot/gshark_scandemo/GsharkScanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impot.gshark_scandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsharkScanDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
